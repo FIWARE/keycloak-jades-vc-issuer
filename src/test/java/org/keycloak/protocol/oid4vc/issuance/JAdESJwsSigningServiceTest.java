5e86660a91e1dfb92a64699a3bb26d891570974f
Dennis Wendland <dennis.wendland@fiware.org>
2024-05-23 10:06:53 +0200

    Fix issuer subject name

--- src/test/java/org/keycloak/protocol/oid4vc/issuance/JAdESJwsSigningServiceTest.java
+++ src/test/java/org/keycloak/protocol/oid4vc/issuance/JAdESJwsSigningServiceTest.java
@@ -8,6 +8,7 @@ import org.bouncycastle.asn1.x509.Extension;
 import org.bouncycastle.cert.CertIOException;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder;
 import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.OperatorCreationException;
@@ -102,7 +103,8 @@ public class JAdESJwsSigningServiceTest {
             issuerSubjectDN = subjectDN;
             issuerKey = key;
         } else {
-            issuerSubjectDN = new X500Name(issuer.cert.getSubjectX500Principal().getName());
+            //issuerSubjectDN = new X500Name(issuer.cert.getSubjectX500Principal().getName());
+            issuerSubjectDN = new JcaX509CertificateHolder((X509Certificate) issuer.cert).getSubject();
             issuerKey = issuer.key;
         }
 
